8395027f7f6b581fbb330de60c538e5819b03079
Patrick Bardo <tech@leaderlabsverige.se>
2023-02-12 18:41:21 +0100

    Github actions commit

--- theme/astro.config.mjs
+++ theme/astro.config.mjs
@@ -6,10 +6,8 @@ import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://odyssey-theme.littlesticks.dev/',
-  // Your public domain, e.g.: https://my-site.dev/. Used to generate sitemaps and canonical URLs.
-  sitemap: true,
-  // Generate sitemap (set to "false" to disable)
+    site: 'https://leader-lab.github.io',
+    base: '/LL-site',
   integrations: [sitemap(), mdx(), image(), lit()], // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
